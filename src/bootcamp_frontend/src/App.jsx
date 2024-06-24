9fdf9270b6271694d820d6d8a170122b431acb95
Dawideg <vaviturz@gmail.com>
2024-06-24 19:07:25 +0200

    second commit

--- src/bootcamp_frontend/src/App.jsx
+++ src/bootcamp_frontend/src/App.jsx
@@ -1,29 +1,11 @@
-import { useState } from 'react';
-import { bootcamp_backend } from 'declarations/bootcamp_backend';
+import { useState } from "react";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    bootcamp_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
+  const [count, SetCount] = useState(0);
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <p>Siema</p>
+      <button onClick={()=>SetCount(count+1)}>{count}</button>
     </main>
   );
 }
